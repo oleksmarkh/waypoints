ec3ebb8e8f106e97ad162e098bb5597f2c66c13d
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2022-02-19 21:33:20 +0100

    catch fetching and parsing errors

--- frontend/src/api/waypoints.ts
+++ frontend/src/api/waypoints.ts
@@ -5,6 +5,14 @@ const { API_URL_ROOT } = config.env
 const endpointUrl = `${API_URL_ROOT}/waypoints/`
 
 export async function retrieveAllWaypoints(): Promise<Waypoint[]> {
-  const response = await fetch(endpointUrl)
-  return response.json() as Promise<Waypoint[]>
+  try {
+    const response = await fetch(endpointUrl)
+    return await response.json() as Waypoint[]
+  } catch (error) {
+    // possible errors: network, API, JSON parsing
+    console.error(error)
+  }
+  return []
 }
+
+// TODO: update and delete
